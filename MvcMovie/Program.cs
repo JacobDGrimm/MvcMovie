8eb9d2fbc439ba8df6b0984e21b3bf4dd9220da6
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-10-22 08:39:46 -0400

    changes the main method in the program class

--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -19,7 +19,26 @@ namespace MvcMovie
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<MvcMovieContext>();
+                    context.Database.Migrate();
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
