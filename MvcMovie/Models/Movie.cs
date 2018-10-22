1075a417a833858c9b3b4d442ad3cc5113dfeb21
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-10-22 10:36:17 -0400

    prettys up the display

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models
 {
@@ -11,9 +12,13 @@ namespace MvcMovie.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
