870a411139b6e5eddde44070f0425e95e1579a52
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-10-22 14:48:20 -0400

    adds the movie genre view model and the appropriaate using statements

---
+++ MvcMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MvcMovie.Models
+{
+    public class MovieGenreViewModel
+    {
+    }
+}
